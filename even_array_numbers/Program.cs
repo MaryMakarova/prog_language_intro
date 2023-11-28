d91c1234a39a2439cbc090b316127b098b1ceaab
Mariia <m.makarova@xsolla.com>
2023-11-28 15:08:37 +0500

    Added folders with solutions

---
+++ even_array_numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+Console.Clear();
+int[] array = new int[10];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next();  
+Console.WriteLine(string.Join(", ", array));
+for (int i = 0; i < array.Length; i++) {
+    if (array[i] % 2 ==0 )
+    count++;
+}
+Console.WriteLine($"Количество четных чисел в массиве: {count}");
