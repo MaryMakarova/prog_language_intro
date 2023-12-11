3f9f9ddea678faa3db2b332874829dfb5f14d7c5
Mariia <m.makarova@xsolla.com>
2023-12-11 18:38:58 +0500

    Added solutions for recursion

---
+++ array_elements_from_end/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте произвольный массив.
+// Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string FlipArray(int[] array, int i)
+        {
+            if (i == 0)
+                return $"{array[i]}";
+            return $"{array[i]}, " + FlipArray(array, i - 1);
+        }
+
+        Console.Clear();
+        Console.Write("Введите длину массива: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        int[] array = new int[x];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            while ((array[i] = new Random().Next(1, 100)) == 0) { }
+        }
+        Console.WriteLine(string.Join(", ", array));
+
+        Console.WriteLine(FlipArray(array, array.Length - 1));
+    }
+}
